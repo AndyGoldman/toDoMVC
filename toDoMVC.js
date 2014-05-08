e6f18d8d580097cf729737ffbced5c6ae161a71a
AndyGoldman <andy.goldman@hotmail.com>
2014-05-07 21:47:59 -0600

    Updated toDoMVC.js

--- toDoMVC.js
+++ toDoMVC.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-	$('.toDoEntry input').keyup(function(e){
+	$('#new-todo').keyup(function(e){
 		if(e.keycode === 13){
 		
-		}
+		};
 		if(e.keycode === 27){
 			
-		}
-	};
+		};
+	}
 });
