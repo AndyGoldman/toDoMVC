ff55ec1fb6c86988254a0badb4850339b96042a8
AndyGoldman <andy.goldman@hotmail.com>
2014-05-08 21:43:28 -0600

    Made updates to toDoMVC based on Ben's code

--- toDoMVC.js
+++ toDoMVC.js
@@ -1,10 +1,51 @@
-$(document).ready(function(){
-	$('#new-todo').keyup(function(e){
-		if(e.keycode === 13){
-		
-		};
-		if(e.keycode === 27){
-			
-		};
-	}
+$(document).ready(function () {
+	$('#new-todo').keyup(function (e) {
+		checkKeyPress(e);
+	});
 });
+									
+function checkKeyPress(e) {
+	if (e.keycode === 13) {
+		insertEntry();
+	} else if (e.keycode === 27) {
+		console.log('esc');
+	}
+}
+
+function insertEntry() {
+	var entry = $('#new-todo').val();
+	$('.template li').clone().appendTo('#toDoList');
+	$('#todo-list li:last-child label').click().text(entry);
+
+	saveEntry(entry);
+		
+}
+
+function uuid() {
+	/*jshint bitwise:false */
+	var i, random;
+	var uuid = '';
+		
+	for (i = 0; i < 32; i++) {
+		random = Math.random() * 16 | 0;
+		if (i === 8 || i === 12 || i === 16 || i === 20) {
+			uuid += '-';
+		}
+		uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString;
+	}
+		
+	return uuid;
+}
+
+function saveEntry(entry) {
+	var id = uuid();
+	var toDoEntry = {
+			'id': id,
+			'entry': entry
+		};
+	localStorage.setItem('Todos', JSON.stringify(toDoEntry));
+}
+
+setListeners();
+
+
